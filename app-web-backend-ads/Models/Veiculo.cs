d09a1c721eb1812e8ecd3c5bee7dc8c843332046
Cristiane de Morais <chryk@hotmail.com>
2022-05-11 16:25:08 +1200

    new feature: adiciona model veiculo

---
+++ app-web-backend-ads/Models/Veiculo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace app_web_backend_ads.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio Informar o Nome")]
+        public string Nome  { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio Informar a Placa")]
+        public string Placa { get; set; }
+    }
+}
