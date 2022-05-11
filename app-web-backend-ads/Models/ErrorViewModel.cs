f3c93fa0ec7eb35f4e10bbfc7664190081fbbfa1
Cristiane de Morais <chryk@hotmail.com>
2022-05-11 16:14:27 +1200

    Adicionar arquivos de projeto.

---
+++ app-web-backend-ads/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace app_web_backend_ads.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
